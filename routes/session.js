cfb7bf913fa6d7d4325b4382df3bce75188374d7
Maxim Kovalov <max.kovalov@gmail.com>
2019-11-26 12:02:50 +0200

    fix(login): user correct path for info

--- routes/session.js
+++ routes/session.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/login', (req, res, next) => {
 	if (req.session.userID) {
-		return res.redirect('/user-info');
+		return res.redirect('/me/info');
 	}
 	res.render('login', {title: 'Login'});
 });
