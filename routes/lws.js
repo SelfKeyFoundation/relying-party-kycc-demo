a2bb8c2eeeb943893891699eb35fc7a06b8a937d
Maxim Kovalov <max.kovalov@gmail.com>
2019-11-30 21:39:20 +0200

    feat(selfkey-node-lib): use node-lib for login

--- routes/lws.js
+++ routes/lws.js
@@ -1,42 +1,41 @@
 var express = require('express');
 var router = express.Router();
 const Users = require('../models/users');
-const kycc = require('../kycc');
+const {getKYCCUserDataForToken} = require('@selfkey/node-lib');
 const LWS_TEMPLATE_ID = '5cf4b5b6fc92bd6e67c72f41';
+const KYCC_API_URL = 'https://dev.instance.kyc-chain.com';
 
 router.post('/login', async (req, res, next) => {
 	const token = req.body.token || req.body.jwt;
 	if (!token) {
 		return next(new Error('Not token provided'));
 	}
-	console.log('XXXXXXXXXXXXXXXXXXXXXXXXx');
 	try {
-		let kyccUser = await kycc.login(token);
-		// TEST GET APPLICATIONS
-		const {payload} = kycc.parseJWT(token);
-		console.log('XXX TOKEN PAYLOAD', payload);
-		try {
-			const applications = await kycc.fetchApplications(LWS_TEMPLATE_ID, kyccUser._id);
-			console.log('XXX', 'KYCC Applications', applications);
-			if (applications.length) {
-				const application = await kycc.fetchApplicationDetails(applications[0].id);
-				console.log('XXX', application);
-			}
-		} catch (error) {
-			console.error('XXX', 'KYCC Applications ERROR', error);
-		}
+		const fileProcessor = {
+			stream: false,
+			process: (file, id) => 'processed file url :)'
+		};
 
-		// TEST
+		let kyccUser = await getKYCCUserDataForToken(token, {
+			fileProcessor,
+			templateId: LWS_TEMPLATE_ID,
+			instanceUrl: KYCC_API_URL
+		});
 
-		let user = Users.findOneByKyccId(kyccUser._id);
+		let user = Users.findOneByKyccId(kyccUser.id);
 		if (!user) {
-			user = Users.create({kyccId: kyccUser._id, name: `test ${Math.random()}`});
+			user = Users.create({
+				kyccId: kyccUser.id,
+				firstName: kyccUser.attributes.firstName.value,
+				lastName: kyccUser.attributes.lastName.value,
+				email: kyccUser.attributes.email.value
+			});
 		}
 		req.session.userID = user.id;
 		return res.json({redirectTo: '/me/info'});
 	} catch (error) {
-		console.error('Token parse', error);
-		return next(new Error('Invalid token provided'));
+		console.error('LWS LOGIN ERROR', error);
+		return next(new Error('Could not login with provided token'));
 	}
 });
 
